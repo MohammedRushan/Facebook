b33e12484a28a5a40df466a753157af72eb127ab
Muhajir M.R <mohamemr@amazon.com>
2021-05-06 19:09:24 -0700

    login page

---
+++ src/test/java/org/facebook/Facebook.java
@@ -0,0 +1,12 @@
+package org.facebook;
+
+import org.baseclass.BaseClass;
+
+public class Facebook extends BaseClass {
+	public static void main(String[] args) {
+		getDriver();
+		launchUrl("www.facebook.com");
+		enterText(findElementId("email"), "rushan");
+		enterText(findElementId("pass"), "rushan");
+	}
+}
